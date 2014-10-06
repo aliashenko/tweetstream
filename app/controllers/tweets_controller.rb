46bd4c1bd4c1a1951d7c0d5b4306a8d0e04093ae
Anna Romanova <lyashenko.ann@gmail.com>
2014-10-06 14:52:43 +0300

    Fix Tweetstream config

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -7,7 +7,7 @@ class TweetsController < ApplicationController
   # GET /tweets.json
   def index
 
-    # TweetStream.configure do |config|  
+    # TweetStream.configure do |config|
     #   config.consumer_key       = 'T0KVGd4LFAVrhbIybKMASOPqg'
     #   config.consumer_secret    = 'hZPc2kVgc5WywHqHQ3dJtlzkCmTS7YWByerDnSYOFHIkqcmxFC'
     #   config.oauth_token        = '1229972551-2PBnleMK6uuzySrXbUkFO4UcNbc0SMgQXt1j7Qi'
