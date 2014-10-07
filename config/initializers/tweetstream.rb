cc35aade3ce32cb522109dbe51dba1631505ca4a
Anna Romanova <lyashenko.ann@gmail.com>
2014-10-07 12:32:08 +0300

    Add tweetsream config

---
+++ config/initializers/tweetstream.rb
@@ -0,0 +1,9 @@
+require 'tweetstream'
+
+TweetStream.configure do |config|
+  config.consumer_key       = 'T0KVGd4LFAVrhbIybKMASOPqg'
+  config.consumer_secret    = 'hZPc2kVgc5WywHqHQ3dJtlzkCmTS7YWByerDnSYOFHIkqcmxFC'
+  config.oauth_token        = '1229972551-2PBnleMK6uuzySrXbUkFO4UcNbc0SMgQXt1j7Qi'
+  config.oauth_token_secret = 'dpl02FjuSPeYlIiOaP8yV3TB31XCeulqiEtxvydGu2N4J'
+  config.auth_method        = :oauth
+end
